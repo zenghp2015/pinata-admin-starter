092ad71ee5699d94abdd07c5a7c22e88c52718f3
zeng.hongpeng <zeng.hongpeng_wb2017@hellogroup.com>
2023-02-14 22:00:33 +0800

    feat

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'VitePress',
+  description: 'Just playing around.',
+  head: [['meta', { name: 'theme-color', content: '#3c8772' }]],
+
+  themeConfig: {
+    nav: [
+      { text: '指南', link: '/guide' },
+      { text: '配置', link: '/guide' },
+      { text: '插件', link: '/guide' },
+      { text: '主题', link: '/guide' },
+      { text: '生态系统', link: '/guide' },
+      { text: '开源项目', link: '/guide' },
+    ],
+    sidebar: [],
+    editLink: {
+      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'twitter', link: '...' },
+    ],
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-present Evan You'
+    }
+  },
+
+ 
+
+  // vite配置
+  vite: {
+    server: {
+      host: '0.0.0.0',
+      port:5210
+    }
+  }
+})
