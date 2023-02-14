7df4d069493c843df3a89aef0a1f81834ba65a84
zenghp2015 <zenghp2015@qq.com>
2023-02-14 23:13:17 +0800

    feat

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,24 +1,20 @@
 import { store, useConfigStore } from "@/store";
-
 const getFileName = (file: string, extname = ".ts") => {
   const fielName = file.substring(file.lastIndexOf("/") + 1);
   return fielName.replace(extname, "");
 };
-
 export const initConfig = () => {
   const configStore = useConfigStore(store);
   const modules = import.meta.glob(["@/config/*.config.ts"], {
     eager: true,
     import: "default",
   });
-
   Object.keys(modules).forEach((key) => {
     const mod = modules[key] || {};
     const modKey = getFileName(key, ".config.ts");
     configStore.config[modKey] = mod;
   });
 };
-
 export default {
   install: initConfig,
 };
