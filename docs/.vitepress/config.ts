b088d78790bd4f5c767dc446e2890ad942999c64
zeng.hongpeng <zeng.hongpeng_wb2017@hellogroup.com>
2023-02-15 21:31:04 +0800

    feat

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,36 +1,58 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  title: 'VitePress',
+  lang: 'zh-CN',
+  base: '/docs',
+  title: 'pinata-admin',
   description: 'Just playing around.',
   head: [['meta', { name: 'theme-color', content: '#3c8772' }]],
-
+  lastUpdated: true,
   themeConfig: {
+    lastUpdatedText: "更新时间",
     nav: [
-      { text: '指南', link: '/guide/' },
+      { text: '指南', link: '/guide/started' },
       { text: '配置', link: '/config/' },
-      // { text: '插件', link: '/guide' },
-      // { text: '主题', link: '/guide' },
-      // { text: '生态系统', link: '/guide' },
-      // { text: '开源项目', link: '/guide' },
+      { text: '插件', link: '/guide' },
+      { text: '主题', link: '/guide' },
+      { text: '生态系统', link: '/guide' },
+      { text: '开源项目', link: '/guide' },
     ],
     sidebar: {
       '/guide/': [
         {
-          text: '指南',
-          collapsed: false,
-          items: []
+          text: '引导',
+          items: [
+            { text: '更新日志', link: '/changelog'},
+            {text: '常见问题', link: '/faq'}
+          ]
         },
-        { text: '快速开始', link: '/guide/' },
-        { text: '路由和菜单', link: '/guide/one' },
-        { text: '权限控制', link: '/guide/two' },
-        { text: '项目构建', link: '/guide/two' },
-        { text: '项目部署', link: '/guide/two' },
-        { text: '个性化配置', link: '/guide/two' },
+        {
+          text: '上手',
+          items: [
+            { text: '快速开始', link: '/guide/started' },
+          ]
+        },
+        {
+          text: '开发',
+          items: [
+            { text: '进入开发', link: '/guide/develop' },
+            { text: '路由和菜单', link: '/guide/one' },
+            { text: '接口和数据', link: '/guide/mock'},
+            { text: '权限控制', link: '/guide/two' },
+            { text: '个性化配置', link: '/guide/two' },
+            { text: '状态管理', link: '/guide/pinia' },
+            { text: '国际化', link: '/guide/i18n' },
+          ]
+        },
+        { text: '构建部署', items: [
+          { text: '项目构建', link: '/guide/two' },
+          { text: '项目部署', link: '/guide/two' },
+        ]},
       ],
       '/config/': [
         {
           text: '配置',
+          collapsed: false,
           items: [
             { text: '配置1', link: '/config/' },
             { text: '配置2', link: '/config/three' },
@@ -41,7 +63,7 @@ export default defineConfig({
     },
     editLink: {
       pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
-      text: 'Edit this page on GitHub'
+      text: '为此页提供修改建议'
     },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
@@ -50,12 +72,9 @@ export default defineConfig({
 
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2019-present Evan You'
+      copyright: 'Copyright © 2022-present pinata-admin-starter'
     }
   },
-
- 
-
   // vite配置
   vite: {
     server: {
--- docs/guide/index.md
+++ docs/guide/index.md
@@ -1,1 +1,13 @@
 # 指南
+
+return Promise.all([query1.run(), query2.run()]). h(
+  data => join(data[0], data[1]),
+  error => {}
+);
+
+function join(players, teams) {
+  return players.map(player => {
+    const team = teams.find(t => player.tid === t.tid);
+    return { ...player, ...team };
+  });
+}
